22b856a7ba3ecbf7d7ce81e5f9dde05c67f64446
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-18 14:42:55 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const socket = require('socket.io');
+const app = express();
+const PORT = 8000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+app.use(express.static('public'));
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('Socket connection made', socket.id);
+
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
+});
