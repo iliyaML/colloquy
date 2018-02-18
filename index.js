46965081fc9d5bdc2377bd242d577eb7df5edb6f
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-18 14:45:52 -0600

    Added environment port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const socket = require('socket.io');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
